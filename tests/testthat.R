4f2fd4a2f9e94c02ea0afe08722f19bf6ca37492
Jian Yen <jdl.yen@gmail.com>
2020-05-26 16:42:22 +1000

    add skeleton test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(aae.pop)
+
+test_check("aae.pop")
---
+++ tests/testthat/test_simulate.R
@@ -0,0 +1,10 @@
+context("simulate")
+
+# setup: simulate some data to test with
+
+
+test_that("simulate returns correct abundances with different processes", {
+
+
+
+})
