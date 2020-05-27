da28fbb8373e13cb4598d6077aefe570d0dd8c94
Jian Yen <jdl.yen@gmail.com>
2020-05-27 10:27:54 +1000

    update templates for mc

--- R/templates.R
+++ R/templates.R
@@ -32,16 +32,31 @@ NULL
 #'   Macquarie perch (*Macquaria australasica*), Australian smelt
 #'   (*Retropinna semonia*), and common galaxias (*Galaxias maculatus*).
 #'
+#'   The \code{get_template} function can be used with user-defined
+#'   templates. Templates must be functions with a name defined by
+#'   \code{template_} followed by a species name or identifier (e.g.
+#'   \code{template_my_species}). These templates must return a named
+#'   list with at least a population matrix model (\code{matrix}), and
+#'   other arguments to pass to \code{\link{dynamics}}. Terms
+#'   specifying \code{covariates} would typically be defined
+#'   in a template with the function only (\code{covariate_function}),
+#'   so that templates can be used with different covariate sets.
+#'   If defined in this way, \code{get_template("my_species")} will
+#'   return a compiled dynamics object for my_species.
+#'
 #' @examples
 #' # add
 get_template <- function(sp, x = NULL, ...) {
 
-  # draw up relevant parameters
-  all_parameters <- templates(sp)
+  # draw up relevant parameters based on corrected species name
+  sp <- parse_species(sp)
+  all_parameters <- get(paste0("template_", sp))
 
   # optional: add covariates
-  if (!is.null(x))
-    all_parameters$covariates <- covariates(x = x, fun = all_parameters$covariate_function)
+  if (!is.null(x)) {
+    all_parameters$covariates <-
+      covariates(x = x, fun = all_parameters$covariate_function)
+  }
 
   # remove covariate_function from all_parameters in all cases
   all_parameters$covariate_function <- NULL
@@ -63,118 +78,253 @@ murraycod <- function(x = NULL, ...) {
   get_template(sp = "murraycod", x = x, ...)
 }
 
-# internal information for dynamics templates
-templates <- function(sp) {
+# internal function: define species defaults
+template_murraycod <- function() {
 
-  # define templates for each species
-
-  # Murray cod
+  # how many stages are we going to work with?
   nstage <- 25
-  mc_matrix <- matrix(0, nrow = nstage, ncol = nstage)
-  mc_matrix[transition(mc_matrix)] <- c(
+
+  # define base matrix
+  mat <- matrix(0, nrow = nstage, ncol = nstage)
+  survival_mask <- combine_masks(
+    transition(mat), col(mat) == row(mat) & col(mat) == nstage
+  )
+  mat[survival_mask] <- c(
     0.4790, 0.5846, 0.6552, 0.7054, 0.7431, 0.7722, 0.7954, 0.8144, 0.8301,
     0.8434, 0.8547, 0.8646, 0.8731, 0.8807, 0.8874, 0.8934, 0.8988, 0.9037,
-    0.9081, 0.9121, 0.9158, 0.9192, 0.9224, 0.9253
+    0.9081, 0.9121, 0.9158, 0.9192, 0.9224, 0.9253, 0.9375
   )
-  mc_matrix[nstage, nstage] <- 0.9375
-  survival_to_yoy <- 0.5 * 0.0122 * 0.1225
-  mc_matrix[reproduction(mc_matrix, dims = c(5:nstage))] <-
-    survival_to_yoy * c(
-    3000, 5000, 7000, 9000, 12000, 16000, 20000, 25000, 30000, 34000,
-    38000, 41000, 43000, 45000, 47000, 48000, 48000, 49000, 49000,
-    49000, 50000
-  )
-  mc_dd <- beverton_holt(
+  mat[nstage, nstage] <- 0.9375
+  yoy_surv <- 0.5 * 0.0122 * 0.1225
+  reproduction_mask <- reproduction(mat, dims = c(5:nstage))
+  mat[reproduction_mask] <- yoy_surv * c(
+      3000, 5000, 7000, 9000, 12000, 16000,
+      20000, 25000, 30000, 34000, 38000,
+      41000, 43000, 45000, 47000, 48000,
+      48000, 49000, 49000, 49000, 50000
+    )
+
+  # define basic BH density dependence
+  dd <- beverton_holt(
     masks = reproduction(mc_matrix, dims = c(5:nstage)),
     params = list(K = 20000)
   )
-  ## PICK UP HERE AND WORK IT OUT
-  mc_covs <- function(mat, x) {
-    mat[transition(mat)] <- mat[transition(mat)] * (1 / (1 + exp(-5 * x)))
+
+  # work out more complex, biomass-based density dependence
+  #  (clustered if/else statements)
+  # mask is survival, function is ifelse(K < summed_abunds, K / summed_abunds, 1)
+  # Variation is in class clusters, e.g. 3+4, 5-7, 8-10, 11-14, 15-25. Clusters
+  #   affect all things younger but nothing older. (e.g. 3+4 = sum(abunds[3:nstage]),
+  #   8-10 = sum(abunds[8:nstage]))
+
+  # basic single variable covariate function
+  covs <- function(mat, x) {
+    mat[transition(mat)] <- mat[transition(mat)] * (1 / (1 + exp(-0.5 * x)))
   }
-  murraycod <- list(
-    matrix = murraycod_matrix,
-    covariate_function = mc_covs,
-    environmental_stochasticity = NULL, # define from SDs of parameters
-    demographic_stochasticity = NULL,   # think about ways to use rbin()
-    density_dependence = mc_dd,
+
+  # define environmental stochasticity based on known standard deviations of
+  #   parameters
+  # survival
+  survival_env <- function(x) {
+
+    # define base parameters for SD
+    sd_scale <- c(0.2, rep(0.15, 3), rep(0.1, 3), rep(0.075, 3), rep(0.05, 15))
+    sd_tmp <- sd_scale * c(
+      0.4790, 0.5846, 0.6552, 0.7054, 0.7431, 0.7722, 0.7954, 0.8144, 0.8301,
+      0.8434, 0.8547, 0.8646, 0.8731, 0.8807, 0.8874, 0.8934, 0.8988, 0.9037,
+      0.9081, 0.9121, 0.9158, 0.9192, 0.9224, 0.9253, 0.9375
+    )
+
+    # simulate normal random variates
+    out <- rnorm(length(x), mean = x, sd = sd_tmp)
+
+    # check none sit outside 0/1
+    out <- ifelse(out > 1, 1, out)
+    out <- ifelse(out < 0, 0, out)
+
+    # return
+    out
+
+  }
+
+  # reproduction
+  reproduction_env <- function(x) {
+
+    # define base parameters for SD
+    yoy_surv <- 0.5 * 0.0122 * 0.1225
+    sd_tmp <- yoy_surv * c(
+      1500, 2400, 3200, 4000, 5300, 6900,
+      8400, 10500, 12600, 13900, 15600,
+      16800, 17600, 18500, 18800, 19200,
+      19200, 19600, 19600, 19600, 20000
+    )
+
+    # simulate normal random variates
+    rnorm(length(x), mean = x, sd = sd_tmp)
+
+    # check none are negative (but can be > 1)
+    out <- ifelse(out < 0, 0, out)
+
+    # return
+    out
+
+  }
+
+  # collate into a enviro stochasticity object
+  envstoch <- environmental_stochasticity(
+    masks = list(survival_mask, reproduction_mask),
+    funs = list(survival_env, reproduction_env)
+  )
+
+  # set demographic stochasticity
+  # survival
+  demo_fn <- function(x) {
+    rpois(length(x), lambda = x)
+  }
+  demostoch <- demographic_stochasticity(
+    masks = combine_masks(survival_mask, reproduction_mask),
+    funs = demo_fn
+  )
+
+  # return template
+  list(
+    matrix = mat,
+    covariate_function = covs,
+    environmental_stochasticity = envstoch,
+    demographic_stochasticity = demostoch,
+    density_dependence = dd,
     density_dependence_n = NULL
   )
-
-  # trout cod
-  troutcod <- list(
-    matrix = NULL,
-    covariate_function = NULL,
-    environmental_stochasticity = NULL,
-    demographic_stochasticity = NULL,
-    density_dependence = NULL,
-    density_dependence_n = NULL
-  )
-
-  # golden perch
-  goldenperch <- list(
-    matrix = NULL,
-    covariate_function = NULL,
-    environmental_stochasticity = NULL,
-    demographic_stochasticity = NULL,
-    density_dependence = NULL,
-    density_dependence_n = NULL
-  )
-
-  # silver perch
-  silverperch <- list(
-    matrix = NULL,
-    covariate_function = NULL,
-    environmental_stochasticity = NULL,
-    demographic_stochasticity = NULL,
-    density_dependence = NULL,
-    density_dependence_n = NULL
-  )
-
-  # Macquarie perch
-  macquarieperch <- list(
-    matrix = NULL,
-    covariate_function = NULL,
-    environmental_stochasticity = NULL,
-    demographic_stochasticity = NULL,
-    density_dependence = NULL,
-    density_dependence_n = NULL
-  )
-
-  # Australian smelt
-  australiansmelt <- list(
-    matrix = NULL,
-    covariate_function = NULL,
-    environmental_stochasticity = NULL,
-    demographic_stochasticity = NULL,
-    density_dependence = NULL,
-    density_dependence_n = NULL
-  )
-
-  # common galaxias
-  commongalaxias <- list(
-    matrix = NULL,
-    covariate_function = NULL,
-    environmental_stochasticity = NULL,
-    demographic_stochasticity = NULL,
-    density_dependence = NULL,
-    density_dependence_n = NULL
-  )
-
-  # collate all into a single list
-  template_list <- list(
-    "murraycod" = murraycod,
-    "troutcod" = troutcod,
-    "goldenperch" = goldenperch,
-    "silverperch" = silverperch,
-    "macquarieperch" = macquarieperch,
-    "australiansmelt" = australiansmelt,
-    "commongalaxias" = commongalaxias
-  )
-
-  # return
-  template_list[sp]
 
 }
 
+# internal function: define species defaults
+template_troutcod <- function() {
 
+  # return template
+  list(
+    matrix = NULL,
+    covariate_function = NULL,
+    environmental_stochasticity = NULL,
+    demographic_stochasticity = NULL,
+    density_dependence = NULL,
+    density_dependence_n = NULL
+  )
+
+}
+
+# internal function: define species defaults
+template_goldenperch <- function() {
+
+  # return template
+  list(
+    matrix = NULL,
+    covariate_function = NULL,
+    environmental_stochasticity = NULL,
+    demographic_stochasticity = NULL,
+    density_dependence = NULL,
+    density_dependence_n = NULL
+  )
+
+}
+
+# internal function: define species defaults
+template_silverperch <- function() {
+
+  # return template
+  list(
+    matrix = NULL,
+    covariate_function = NULL,
+    environmental_stochasticity = NULL,
+    demographic_stochasticity = NULL,
+    density_dependence = NULL,
+    density_dependence_n = NULL
+  )
+
+}
+
+# internal function: define species defaults
+template_macquarieperch <- function() {
+
+  # return template
+  list(
+    matrix = NULL,
+    covariate_function = NULL,
+    environmental_stochasticity = NULL,
+    demographic_stochasticity = NULL,
+    density_dependence = NULL,
+    density_dependence_n = NULL
+  )
+
+}
+
+# internal function: define species defaults
+template_australiansmelt <- function() {
+
+  # return template
+  list(
+    matrix = NULL,
+    covariate_function = NULL,
+    environmental_stochasticity = NULL,
+    demographic_stochasticity = NULL,
+    density_dependence = NULL,
+    density_dependence_n = NULL
+  )
+
+}
+
+# internal function: define species defaults
+template_commongalaxias <- function() {
+
+  # return template
+  list(
+    matrix = NULL,
+    covariate_function = NULL,
+    environmental_stochasticity = NULL,
+    demographic_stochasticity = NULL,
+    density_dependence = NULL,
+    density_dependence_n = NULL
+  )
+
+}
+
+# convert species names to standardised common names
+#  (could replace with sci names if ambiguous)
+parse_species <- function(sp) {
+
+  # create lookup table with common synonyms
+  sp_list <- list(
+    "murraycod" = "murraycod",
+    "maccullochellapeelii" = "murraycod",
+    "troutcod" = "troutcod",
+    "maccullochellamacquariensis" = "troutcod",
+    "goldenperch" = "goldenperch",
+    "macquariaambigua" = "goldenperch",
+    "silverperch" = "silverperch",
+    "bidyanusbidyanus" = "silverperch",
+    "macquarieperch" = "macquarieperch",
+    "macquariaaustralasica" = "macquarieperch",
+    "australiansmelt" = "australiansmelt",
+    "retropinnasemoni" = "australiansmelt",
+    "commongalaxias" = "commongalaxias",
+    "galaxiasmaculatus" = "commongalaxias"
+  )
+
+  # clean sp to remove underscores or spaces or hyphens
+  sp_clean <- gsub("_|-| ", "", sp)
+
+  # pull out a match if there, otherwise check the template
+  #   function exists and return as is (error otherwise)
+  if (sp_clean %in% names(sp_list)) {
+    sp <- sp_list[sp_clean]
+  } else {
+    if(!exists(paste0("template_", sp))) {
+      stop(sp, " does not have a matching template function",
+           call. = FALSE)
+    }
+  }
+
+  # return appropriate species
+  sp
+
+}
