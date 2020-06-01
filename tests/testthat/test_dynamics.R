568b0ed603a89a911f2e0850eb2dba81120d8027
Jian Yen <jdl.yen@gmail.com>
2020-06-01 14:45:59 +1000

    update tests to cover more processes and dynamics

---
+++ tests/testthat/test_dynamics.R
@@ -0,0 +1,152 @@
+context("dynamics")
+
+# setup: simulate some data to test with
+nstage <- 5
+mat <- matrix(0, nrow = nstage, ncol = nstage)
+mat[reproduction(mat, dims = 4:5)] <- rpois(2, 20)
+mat[survival(mat)] <- plogis(rnorm(nstage))
+mat[transition(mat)] <- plogis(rnorm(nstage - 1))
+
+# add covariate effects
+ntime <- 35
+xsim <- rnorm(ntime)
+cov_fn <- function(popmat, x) {
+  popmat[survival(popmat)] <- popmat[survival(popmat)] * plogis(x)
+  popmat
+}
+cov_eff <- covariates(x = xsim,
+                      fun = cov_fn)
+
+# add density dependence
+dd_masks <- list(reproduction(mat, dims = 4:5))
+dd_fns <- list(
+  function(x, n) x * (1 / (1 + 1e-4 * sum(n)))
+)
+dd <- density_dependence(masks = dd_masks, funs = dd_fns)
+
+# add demographic stochasticity
+demo_mask <- all_stages(mat)
+demo_fn <- function(x) x + 1
+demostoch <- demographic_stochasticity(masks = demo_mask, funs = demo_fn)
+
+# add environmental stochasticity
+mask_list <- list(reproduction(mat, dims = 4:5), survival(mat))
+fn_list <- list(
+  function(x) x + 2,
+  function(x) x + 0.01
+)
+envstoch <- environmental_stochasticity(masks = mask_list, funs = fn_list)
+
+# add rescaling method (alternative form of density dependence)
+rescale_mask <- all_stages(mat)
+rescale_fn <- function(x) 200 * (x / sum(x))
+resc <- density_dependence_n(rescale_mask, rescale_fn)
+
+# define simulation settings
+nsim <- 10
+ntime <- 50
+
+test_that("dynamics object returns correct processes for different combinations of processes", {
+
+  # test basic dynamics object with no additional processes
+  dyn_obj <- dynamics(mat)
+  expect_equal(dyn_obj$base_matrix, dyn_obj$matrix)
+  expect_equal(dyn_obj$matrix, mat)
+  expect_equal(dyn_obj$nclass, ncol(mat))
+  expect_equal(dyn_obj$nspecies, 1L)
+  expect_equal(dyn_obj$ntime, 1L)
+  expect_null(dyn_obj$covariates)
+  expect_null(dyn_obj$environmental_stochasticity)
+  expect_null(dyn_obj$demographic_stochasticity)
+  expect_null(dyn_obj$density_dependence)
+  expect_null(dyn_obj$density_dependence_n)
+
+  # test basic dynamics object with covariates
+  dyn_obj <- dynamics(mat, cov_eff)
+  expect_equal(dyn_obj$base_matrix, mat)
+  expect_length(dyn_obj$matrix, cov_eff$ntime)
+  expect_equal(dyn_obj$nclass, ncol(mat))
+  expect_equal(dyn_obj$nspecies, 1L)
+  expect_equal(dyn_obj$ntime, cov_eff$ntime)
+  expect_equal(dyn_obj$covariates, cov_eff)
+  expect_null(dyn_obj$environmental_stochasticity)
+  expect_null(dyn_obj$demographic_stochasticity)
+  expect_null(dyn_obj$density_dependence)
+  expect_null(dyn_obj$density_dependence_n)
+
+  # test basic dynamics object with environmental stochasticity
+  dyn_obj <- dynamics(mat, envstoch)
+  expect_equal(dyn_obj$base_matrix, mat)
+  expect_equal(dyn_obj$matrix, mat)
+  expect_equal(dyn_obj$nclass, ncol(mat))
+  expect_equal(dyn_obj$nspecies, 1L)
+  expect_equal(dyn_obj$ntime, 1L)
+  expect_null(dyn_obj$covariates)
+  expect_equal(dyn_obj$environmental_stochasticity, envstoch)
+  expect_null(dyn_obj$demographic_stochasticity)
+  expect_null(dyn_obj$density_dependence)
+  expect_null(dyn_obj$density_dependence_n)
+
+  # test basic dynamics object with demographic stochasticity
+  dyn_obj <- dynamics(mat, demostoch)
+  expect_equal(dyn_obj$base_matrix, mat)
+  expect_equal(dyn_obj$matrix, mat)
+  expect_equal(dyn_obj$nclass, ncol(mat))
+  expect_equal(dyn_obj$nspecies, 1L)
+  expect_equal(dyn_obj$ntime, 1L)
+  expect_null(dyn_obj$covariates)
+  expect_null(dyn_obj$environmental_stochasticity)
+  expect_equal(dyn_obj$demographic_stochasticity, demostoch)
+  expect_null(dyn_obj$density_dependence)
+  expect_null(dyn_obj$density_dependence_n)
+
+  # test basic dynamics object with density_dependence
+  dyn_obj <- dynamics(mat, dd)
+  expect_equal(dyn_obj$base_matrix, mat)
+  expect_equal(dyn_obj$matrix, mat)
+  expect_equal(dyn_obj$nclass, ncol(mat))
+  expect_equal(dyn_obj$nspecies, 1L)
+  expect_equal(dyn_obj$ntime, 1L)
+  expect_null(dyn_obj$covariates)
+  expect_null(dyn_obj$environmental_stochasticity)
+  expect_null(dyn_obj$demographic_stochasticity)
+  expect_equal(dyn_obj$density_dependence, dd)
+  expect_null(dyn_obj$density_dependence_n)
+
+  # test basic dynamics object with rescale density dependence
+  dyn_obj <- dynamics(mat, resc)
+  expect_equal(dyn_obj$base_matrix, mat)
+  expect_equal(dyn_obj$matrix, mat)
+  expect_equal(dyn_obj$nclass, ncol(mat))
+  expect_equal(dyn_obj$nspecies, 1L)
+  expect_equal(dyn_obj$ntime, 1L)
+  expect_null(dyn_obj$covariates)
+  expect_null(dyn_obj$environmental_stochasticity)
+  expect_null(dyn_obj$demographic_stochasticity)
+  expect_null(dyn_obj$density_dependence)
+  expect_equal(dyn_obj$density_dependence_n, resc)
+
+})
+
+
+test_that("dynamics object errors informatively with unsuitable processes", {
+
+  # non-process passed to dynamics
+  expect_error(
+    dynamics(mat, rnorm(10)),
+    "must be one of covariates, environmental_stochasticity"
+  )
+
+  # multiples of the same process
+  expect_error(
+    dynamics(mat, dd, dd),
+    "Multiple objects provided for the following processes: density_dependence"
+  )
+
+  # multiples of the same two processes
+  expect_error(
+    dynamics(mat, dd, dd, demostoch, demostoch),
+    "Multiple objects provided for the following processes: demographic_stochasticity, and density_dependence"
+  )
+
+})
--- tests/testthat/test_simulate.R
+++ tests/testthat/test_simulate.R
@@ -26,14 +26,14 @@ dd <- density_dependence(masks = dd_masks, funs = dd_fns)
 
 # add demographic stochasticity
 demo_mask <- all_stages(mat)
-demo_fn <- function(x) rpois(length(x), lambda = x)
+demo_fn <- function(x) x + 1
 demostoch <- demographic_stochasticity(masks = demo_mask, funs = demo_fn)
 
 # add environmental stochasticity
 mask_list <- list(reproduction(mat, dims = 4:5), survival(mat))
 fn_list <- list(
-  function(x) x + rpois(length(x), lambda = 2),
-  function(x) x + runif(length(x), min = -min(x), max = max(1 - x))
+  function(x) x + 2,
+  function(x) x + 0.01
 )
 envstoch <- environmental_stochasticity(masks = mask_list, funs = fn_list)
 
@@ -42,21 +42,13 @@ rescale_mask <- all_stages(mat)
 rescale_fn <- function(x) 200 * (x / sum(x))
 resc <- density_dependence_n(rescale_mask, rescale_fn)
 
-# manual simulation function
-sim_manual <- function(mat, ntime, nsim, init) {
-  nstage <- ncol(mat)
-  out <- array(NA, dim = c(nsim, nstage, ntime + 1))
-  out[, , 1] <- init
-  for (i in seq_len(ntime))
-    out[, , (i + 1)] <- floor(out[, , i] %*% t(mat))
-  out
-}
+# define simulation settings
+nsim <- 10
+ntime <- 50
 
-test_that("simulate returns correct abundances with different processes", {
+test_that("simulate returns correct abundances without extra processes", {
 
   # simulate trajectories with no uncertainty
-  nsim <- 10
-  ntime <- 50
   dyn <- dynamics(mat)
   init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
   value <- simulate(
@@ -65,9 +57,10 @@ test_that("simulate returns correct abundances with different processes", {
     init = init_set,
     options = list(ntime = ntime, tidy_abundances = floor)
   )
-  target <- sim_manual(
-    mat, ntime = ntime, nsim = nsim, init = init_set
-  )
+  target <- array(dim = dim(value))
+  target[, , 1] <- init_set
+  for (i in seq_len(ntime))
+    target[, , (i + 1)] <- floor(target[, , i] %*% t(dyn$matrix))
   class(target) <- c("simulation", "array")
   expect_equal(target, value)
 
@@ -83,5 +76,218 @@ test_that("simulate returns correct abundances with different processes", {
                round(lambda_manual, 4))
   expect_equal(round(Re(lambda_real), 4),
                round(lambda_value, 4))
+
+})
+
+test_that("simulate returns correct abundances with covariates", {
+
+  # simulate trajectories with no uncertainty but with covariates
+  dyn <- dynamics(mat, cov_eff)
+  init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
+  value <- simulate(
+    dyn,
+    nsim = nsim,
+    init = init_set,
+    options = list(ntime = ntime, tidy_abundances = floor)
+  )
+  target <- array(dim = dim(value))
+  target[, , 1] <- init_set
+  for (i in seq_len(dyn$ntime))
+    target[, , i + 1] <- floor(target[, , i] %*% t(dyn$matrix[[i]]))
+  class(target) <- c("simulation", "array")
+  expect_equal(target, value)
+
+})
+
+test_that("simulate returns correct abundances with density dependence and covariates", {
+
+  # simulate trajectories with no uncertainty but with covariates
+  dyn <- dynamics(mat, cov_eff, dd)
+  init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
+  value <- simulate(
+    dyn,
+    nsim = nsim,
+    init = init_set,
+    options = list(ntime = ntime, tidy_abundances = floor)
+  )
+  target <- array(dim = dim(value))
+  target[, , 1] <- init_set
+  dd_manual <- function(x, n) {
+    x[1, 4:5] <- dd_fns[[1]](x[1, 4:5], n)
+    x
+  }
+  for (i in seq_len(dyn$ntime)) {
+    mat_tmp <- lapply(seq_len(nsim), function(x) t(dd_manual(dyn$matrix[[i]], target[x, , i])))
+    target[, , i + 1] <- floor(t(mapply(`%*%`, lapply(seq_len(nsim), function(x) target[x, , i]), mat_tmp)))
+  }
+  class(target) <- c("simulation", "array")
+  expect_equal(target, value)
+
+})
+
+test_that("simulate returns correct abundances with demographic stochasticity and covariates", {
+
+  # simulate trajectories with no uncertainty but with covariates
+  dyn <- dynamics(mat, cov_eff, demostoch)
+  init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
+  value <- simulate(
+    dyn,
+    nsim = nsim,
+    init = init_set,
+    options = list(ntime = ntime, tidy_abundances = floor)
+  )
+  target <- array(dim = dim(value))
+  target[, , 1] <- init_set
+  for (i in seq_len(dyn$ntime))
+    target[, , i + 1] <- floor(target[, , i] %*% t(dyn$matrix[[i]])) + 1
+  class(target) <- c("simulation", "array")
+  expect_equal(target, value)
+
+})
+
+test_that("simulate returns correct abundances with demographic and environmental stochasticity and covariates", {
+
+  # simulate trajectories with no uncertainty but with covariates
+  dyn <- dynamics(mat, cov_eff, demostoch, envstoch)
+  init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
+  value <- simulate(
+    dyn,
+    nsim = nsim,
+    init = init_set,
+    options = list(ntime = ntime, tidy_abundances = floor)
+  )
+  target <- array(dim = dim(value))
+  target[, , 1] <- init_set
+  for (i in seq_len(dyn$ntime)) {
+    mat_tmp <- dyn$matrix[[i]]
+    mat_tmp[1, 4:5] <- mat_tmp[1, 4:5] + 2
+    idx <- row(mat_tmp) == col(mat_tmp)
+    mat_tmp[idx] <- mat_tmp[idx] + 0.01
+    target[, , i + 1] <- floor(target[, , i] %*% t(mat_tmp)) + 1
+  }
+  class(target) <- c("simulation", "array")
+  expect_equal(target, value)
+
+})
+
+test_that("simulate returns correct abundances with rescale density dependence and covariates", {
+
+  # simulate trajectories with no uncertainty but with covariates
+  dyn <- dynamics(mat, cov_eff, resc)
+  init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
+  value <- simulate(
+    dyn,
+    nsim = nsim,
+    init = init_set,
+    options = list(ntime = ntime, tidy_abundances = floor)
+  )
+  target <- array(dim = dim(value))
+  target[, , 1] <- init_set
+  for (i in seq_len(dyn$ntime)) {
+    target[, , i + 1] <- target[, , i] %*% t(dyn$matrix[[i]])
+    target[, , i + 1] <- floor(t(apply(target[, , i + 1], 1, function(x) 200 * (x / sum(x)))))
+  }
+  class(target) <- c("simulation", "array")
+  expect_equal(target, value)
+
+})
+
+test_that("simulate returns correct abundances with density dependence", {
+
+  # simulate trajectories with no uncertainty but with covariates
+  dyn <- dynamics(mat, dd)
+  init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
+  value <- simulate(
+    dyn,
+    nsim = nsim,
+    init = init_set,
+    options = list(ntime = ntime, tidy_abundances = floor)
+  )
+  target <- array(dim = dim(value))
+  target[, , 1] <- init_set
+  dd_manual <- function(x, n) {
+    x[1, 4:5] <- dd_fns[[1]](x[1, 4:5], n)
+    x
+  }
+  for (i in seq_len(ntime)) {
+    mat_tmp <- lapply(seq_len(nsim), function(x) t(dd_manual(dyn$matrix, target[x, , i])))
+    target[, , i + 1] <- floor(t(mapply(`%*%`, lapply(seq_len(nsim), function(x) target[x, , i]), mat_tmp)))
+  }
+  class(target) <- c("simulation", "array")
+  expect_equal(target, value)
+
+})
+
+test_that("simulate returns correct abundances with density dependence and demostoch", {
+
+  # simulate trajectories with no uncertainty but with covariates
+  dyn <- dynamics(mat, dd, demostoch)
+  init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
+  value <- simulate(
+    dyn,
+    nsim = nsim,
+    init = init_set,
+    options = list(ntime = ntime, tidy_abundances = floor)
+  )
+  target <- array(dim = dim(value))
+  target[, , 1] <- init_set
+  dd_manual <- function(x, n) {
+    x[1, 4:5] <- dd_fns[[1]](x[1, 4:5], n)
+    x
+  }
+  for (i in seq_len(ntime)) {
+    mat_tmp <- lapply(seq_len(nsim), function(x) t(dd_manual(dyn$matrix, target[x, , i])))
+    target[, , i + 1] <- floor(t(mapply(`%*%`, lapply(seq_len(nsim), function(x) target[x, , i]), mat_tmp)) + 1)
+  }
+  class(target) <- c("simulation", "array")
+  expect_equal(target, value)
+
+})
+
+test_that("simulate returns correct abundances with density dependence and envstoch", {
+
+  # simulate trajectories with no uncertainty but with covariates
+  dyn <- dynamics(mat, dd, envstoch)
+  init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
+  value <- simulate(
+    dyn,
+    nsim = nsim,
+    init = init_set,
+    options = list(ntime = ntime, tidy_abundances = floor)
+  )
+  target <- array(dim = dim(value))
+  target[, , 1] <- init_set
+  dd_manual <- function(x, n) {
+    x[1, 4:5] <- dd_fns[[1]](x[1, 4:5], n)
+    x
+  }
+  for (i in seq_len(ntime)) {
+    mat_tmp <- dyn$matrix
+    mat_tmp[1, 4:5] <- mat_tmp[1, 4:5] + 2
+    idx <- row(mat_tmp) == col(mat_tmp)
+    mat_tmp[idx] <- mat_tmp[idx] + 0.01
+    mat_tmp <- lapply(seq_len(nsim), function(x) t(dd_manual(mat_tmp, target[x, , i])))
+    target[, , i + 1] <- floor(t(mapply(`%*%`, lapply(seq_len(nsim), function(x) target[x, , i]), mat_tmp)))
+  }
+  class(target) <- c("simulation", "array")
+  expect_equal(target, value)
+
+})
+
+
+test_that("simulate errors informatively when initial values have unsuitable dims", {
+
+  # simulate trajectories with no uncertainty but with covariates
+  dyn <- dynamics(mat, dd, envstoch)
+  init_set <- matrix(rpois(nstage * nsim, lambda = 20), ncol = nstage)
+  expect_error(
+    simulate(
+      dyn,
+      nsim = nsim,
+      init = init_set[-1],
+      options = list(ntime = ntime, tidy_abundances = floor)
+    ),
+    "must have dimensions \\(10,5\\) or \\(5\\)"
+  )
 
 })
