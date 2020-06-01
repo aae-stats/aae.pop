e54a8df2aa3363318673e309c8bc3d2a2f53865c
Jian Yen <jdl.yen@gmail.com>
2020-06-01 15:34:52 +1000

    update tests of mc template with correct order of covariates addition

--- tests/testthat/test_templates.R
+++ tests/testthat/test_templates.R
@@ -9,10 +9,10 @@ test_that("templates return working dynamics objects", {
 
   # expect most processed to be defined
   expect_null(dyn$density_dependence_n)
+  expect_null(dyn$covariates)
   expect_equal(class(dyn$density_dependence), c("density_dependence", "function"))
   expect_equal(class(dyn$demographic_stochasticity), c("demographic_stochasticity", "function"))
   expect_equal(class(dyn$environmental_stochasticity), c("environmental_stochasticity", "function"))
-  expect_equal(class(dyn$covariates), c("covariates", "list"))
 
   # now with covariates
   dyn <- murraycod(x = rnorm(20))
@@ -20,5 +20,8 @@ test_that("templates return working dynamics objects", {
   expect_equal(dim(sim), c(1L, 25L, 21L))
   expect_equal(dyn$base_matrix, murraycod()$matrix)
   expect_length(dyn$matrix, 20L)
+
+  # expect covariates now to be defined
+  expect_equal(class(dyn$covariates), c("covariates", "list"))
 
 })
