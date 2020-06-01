2dcc1c6720c511f70b1d4bf7b97e7b020a1a10bd
Jian Yen <jdl.yen@gmail.com>
2020-06-01 10:43:32 +1000

    add template for multispecies class

--- R/multispecies.R
+++ R/multispecies.R
@@ -1,23 +1,45 @@
-# combine dynamics in a multispecies object
+#' @name multispecies
+#' @title Create a population dynamics object with multiple species
+#' @description Define population dynamics for multiple species from
+#'   a set of single-species \code{\link{dynamics}} objects and
+#'   defined pairwise interactions.
+NULL
+
+#' @rdname multispecies
+#'
+#' @export
 multispecies <- function(interactions, ...) {
 
   # all ... should be dynamics objects
 
+
+  # pull out dynamics$hex for all and use to find unique objects
+
   # set outputs from input dynamics objects
   #  - decide how to handle conflicting settings for input pops
-  list(
-    ntime = defaults$ntime,
-    nclass = nclass_list,
-    nspecies = nspecies,
-    matrix = matrix_list,
-    stochasticity = stochasticity_list,
-    density = density_list,
-    rescale = rescale_list,
-    is_modified = is_modified_list,
-    is_stochastic = is_stochastic_list,
-    is_density = is_density_list,
-    is_rescaled = is_rescaled_list,
-    is_multispecies = TRUE
-  )
+  out <- NULL
 
+  as_multispecies(out)
+
+}
+
+#' @rdname multispecies
+#'
+#' @export
+pairwise_interaction <- function(target, cause, masks, funs) {
+
+  out <- NULL
+
+  as_interaction(out)
+
+}
+
+# internal function: set multispecies class
+as_multispecies <- function(x) {
+  as_class(x, name = "multispecies", type = "list")
+}
+
+# internal function: set interaction class
+as_interaction <- function(x) {
+  as_class(x, name = "interaction", type = "list")
 }
