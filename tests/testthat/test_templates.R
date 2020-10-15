b87a9481ac45ae9d1bd46957f280270b546d5d87
Jian Yen <jdl.yen@gmail.com>
2020-10-15 14:32:30 +1100

    include mp in template test

--- tests/testthat/test_templates.R
+++ tests/testthat/test_templates.R
@@ -14,4 +14,17 @@ test_that("templates return working dynamics objects", {
   expect_equal(class(dyn$demographic_stochasticity), c("demographic_stochasticity", "function"))
   expect_equal(class(dyn$environmental_stochasticity), c("environmental_stochasticity", "function"))
 
+  # simulate from a Murray cod object
+  dyn <- macquarie_perch()
+  arg <- get_args("macquarie_perch")
+  sim <- simulate(dyn, args = arg$static, args.fn = arg$funs)
+  expect_equal(dim(sim), c(1L, 30L, 51L))
+
+  # expect most processes to be defined
+  expect_equal(class(dyn$density_dependence_n), c("density_dependence_n", "function"))
+  expect_equal(class(dyn$covariates), c("covariates", "function"))
+  expect_equal(class(dyn$density_dependence), c("density_dependence", "function"))
+  expect_null(dyn$demographic_stochasticity)
+  expect_equal(class(dyn$environmental_stochasticity), c("environmental_stochasticity", "function"))
+
 })
