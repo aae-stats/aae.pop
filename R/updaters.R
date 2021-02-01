8771622e94e2b6e94e4cba3b3a2f1a0c9435c90f
Jian Yen <jdl.yen@gmail.com>
2021-02-01 16:20:04 +1100

    fix typo in binomial updater where options were incorrectly named in warning message

--- R/updaters.R
+++ R/updaters.R
@@ -54,7 +54,7 @@ update_binomial_leslie <- function(pop, mat) {
 
   if (!all((pop %% 1) == 0)) {
     stop("some abundances are not integers, so cannot be used ",
-         "with update_binomial_leslie. Check options()$tidy_abundances ",
+         "with update_binomial_leslie. Check options()$aae.pop_tidy_abundances ",
          "and update with an appropriate method (e.g. floor)",
          call. = FALSE)
   }
